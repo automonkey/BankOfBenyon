46f3548f0127a65cd0996000584a4c9a999a3ddf
Will Benyon <git@benyon.io>
2016-05-01 09:26:25 +0100

    Add Xcode generated project
    (minus xcuserdata)

---
+++ BankOfBenyon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BankOfBenyon
+//
+//  Created by Will Benyon on 01/05/2016.
+//  Copyright © 2016 Will Benyon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
