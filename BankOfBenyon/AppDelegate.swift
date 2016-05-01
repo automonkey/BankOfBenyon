5de05c696294d8cd69655e703c6e9aabc4cd3b32
Will Benyon <git@benyon.io>
2016-05-01 09:42:11 +0100

    Get rid of pesky storyboard

--- BankOfBenyon/AppDelegate.swift
+++ BankOfBenyon/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  BankOfBenyon
-//
-//  Created by Will Benyon on 01/05/2016.
-//  Copyright © 2016 Will Benyon. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
@@ -15,7 +7,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        if let window = window {
+            window.backgroundColor = UIColor.whiteColor()
+            window.rootViewController = ViewController()
+            window.makeKeyAndVisible()
+        }
         return true
     }
 
@@ -43,4 +40,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
